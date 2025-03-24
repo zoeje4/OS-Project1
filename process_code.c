c66b518aeccf7fb160b8800df2759ccdfd776cf1
Zoe Ellison <zellison1@student.gsu.edu>
2025-03-24 00:16:03 -0400

    Update process_code.c

--- process_code.c
+++ process_code.c
@@ -15,6 +15,8 @@ struct Process {
     int priority;
     int waitingTime;
     int turnaroundTime;
+    int startTime;
+    int completionTime;
 };
 
 // Function to read processes from a file
@@ -33,23 +35,80 @@ vector<Process> readProcesses(const string& filename) {
         stringstream ss(line);
         Process p;
         ss >> p.pid >> p.arrivalTime >> p.burstTime >> p.priority;
+        p.waitingTime = p.turnaroundTime = p.startTime = p.completionTime = 0;
         processes.push_back(p);
     }
     file.close();
     return processes;
 }
 
-// Function to calculate waiting & turnaround times
+// Function to calculate waiting, turnaround, start, and completion times
 void calculateTimes(vector<Process>& processes) {
-    int completionTime = 0;
+    int currentTime = 0;
     for (auto& p : processes) {
-        if (completionTime < p.arrivalTime) {
-            completionTime = p.arrivalTime;
+        if (currentTime < p.arrivalTime) {
+            currentTime = p.arrivalTime;
         }
-        p.waitingTime = completionTime - p.arrivalTime;
-        p.turnaroundTime = p.waitingTime + p.burstTime;
-        completionTime += p.burstTime;
+        p.startTime = currentTime;
+        p.completionTime = currentTime + p.burstTime;
+        p.waitingTime = p.startTime - p.arrivalTime;
+        p.turnaroundTime = p.completionTime - p.arrivalTime;
+        currentTime += p.burstTime;
     }
+}
+
+// Function to display a Gantt chart
+void displayGanttChart(const vector<Process>& processes) {
+    cout << "\nGantt Chart:\n";
+
+    // Top border
+    cout << " ";
+    for (const auto& p : processes) {
+        for (int i = 0; i < p.burstTime; i++) cout << "--";
+        cout << " ";
+    }
+    cout << "\n|";
+
+    // Process execution sequence
+    for (const auto& p : processes) {
+        for (int i = 0; i < p.burstTime - 1; i++) cout << " ";
+        cout << "P" << p.pid;
+        for (int i = 0; i < p.burstTime - 1; i++) cout << " ";
+        cout << "|";
+    }
+    cout << "\n ";
+
+    // Bottom border
+    for (const auto& p : processes) {
+        for (int i = 0; i < p.burstTime; i++) cout << "--";
+        cout << " ";
+    }
+    cout << "\n";
+
+    // Timeline
+    cout << "0";
+    for (const auto& p : processes) {
+        for (int i = 0; i < p.burstTime; i++) cout << "  ";
+        cout << p.completionTime;
+    }
+    cout << "\n";
+}
+
+// Function to calculate and display average WT and TAT
+void displayAverages(const vector<Process>& processes) {
+    double totalWT = 0, totalTAT = 0;
+    int n = processes.size();
+
+    cout << "\nWaiting Time (WT) and Turnaround Time (TAT) for each process:\n";
+    cout << "PID  WT  TAT\n";
+    for (const auto& p : processes) {
+        cout << setw(3) << p.pid << setw(4) << p.waitingTime << setw(5) << p.turnaroundTime << endl;
+        totalWT += p.waitingTime;
+        totalTAT += p.turnaroundTime;
+    }
+
+    cout << "\nAverage Waiting Time (AWT): " << fixed << setprecision(2) << (totalWT / n) << endl;
+    cout << "Average Turnaround Time (ATAT): " << fixed << setprecision(2) << (totalTAT / n) << endl;
 }
 
 // First-Come, First-Served (FCFS) Scheduling
@@ -66,6 +125,9 @@ void fcfsScheduling(vector<Process> processes) {
         cout << setw(3) << p.pid << setw(8) << p.arrivalTime << setw(7)
              << p.burstTime << setw(9) << p.waitingTime << setw(11) << p.turnaroundTime << endl;
     }
+
+    displayGanttChart(processes);
+    displayAverages(processes);
 }
 
 // Shortest Job First (SJF) Scheduling
@@ -82,6 +144,9 @@ void sjfScheduling(vector<Process> processes) {
         cout << setw(3) << p.pid << setw(8) << p.arrivalTime << setw(7)
              << p.burstTime << setw(9) << p.waitingTime << setw(11) << p.turnaroundTime << endl;
     }
+
+    displayGanttChart(processes);
+    displayAverages(processes);
 }
 
 // Main function
